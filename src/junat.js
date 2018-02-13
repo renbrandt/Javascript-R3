7e5f9c6c98fd1a42e7ce1c6324f81a86957e9e9c
Tiinakooo <tiina.hrd@gmail.com>
2018-02-13 16:04:15 +0200

    älkää huomioiko tätä

--- src/junat.js
+++ src/junat.js
@@ -2,6 +2,13 @@
     var xhttp = new XMLHttpRequest();
     var timetableHKI = "";
 
+    function getFile() {
+        var depstation = document.getElementById("depoptions").value;
+        var arrstation = document.getElementById("destoptions").value;
+        xhttp.open("GET", 'https://rata.digitraffic.fi/api/v1/live-trains/station/' + depstation + "/" + arrstation, true);
+        xhttp.send(null);
+
+
     xhttp.onreadystatechange = function(){
         if (xhttp.readyState == 4 && xhttp.status == 200){
 
@@ -13,6 +20,8 @@
 
                 for (i = 0; trains.length; i++){
                     var result = trains[i];
+                    // var destinationIndex = result.timeTableRows[result.timeTableRows[]];
+                    // console.log(destinationIndex);
                     var deptTime = new Date(result.timeTableRows[0].scheduledTime).toLocaleTimeString("fi", {hour: '2-digit', minute:'2-digit', hour12: false});
                     var arrTime = new Date(result.timeTableRows[result.timeTableRows.length-1].scheduledTime).toLocaleTimeString("fi", {hour: '2-digit', minute:'2-digit', hour12: false});
                     var arrStation = (result.timeTableRows[result.timeTableRows.length - 1].stationShortCode);
@@ -22,15 +31,22 @@
                     if (result.timeTableRows[i].type === "DEPARTURE") { //tulostetaan vain departures
                     timetableHKI = timetableHKI + "<li>" + result.timeTableRows[i].type + "Määräasema: " + arrStation + " Lähtöaika: " + deptTime + " Saapumisaika: " + arrTime;
                     }
+
+
                 document.getElementById("list").innerHTML = timetableHKI;
+
+
                 }
+
+
             }
 
 
-    };
 
-    function getFile() {
-    xhttp.open("GET", 'https://rata.digitraffic.fi/api/v1/live-trains/station/HKI/LH', true);
-    xhttp.send(null);
-    
+    };
+    timetableHKI = " ";
     }
+
+
+
+
