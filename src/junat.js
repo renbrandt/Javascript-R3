5c6e76156626369ec8500420802a8d939bfa32ea
Renne Brandt <renne.brandt@iki.fi>
2018-02-14 09:26:24 +0200

    korjattu saapumisaika ja asema

--- src/junat.js
+++ src/junat.js
@@ -24,18 +24,16 @@
                     var index = indexSearch(result);
 
                     var deptTime = new Date(result.timeTableRows[0].scheduledTime).toLocaleTimeString("fi", {hour: '2-digit', minute:'2-digit', hour12: false});
-                    var arrTime = new Date(result.timeTableRows[result.timeTableRows.length-1].scheduledTime).toLocaleTimeString("fi", {hour: '2-digit', minute:'2-digit', hour12: false});
-                    var arrStation = (result.timeTableRows[result.timeTableRows.length - 1].stationShortCode);
+                    var arrTime = new Date(result.timeTableRows[index].scheduledTime).toLocaleTimeString("fi", {hour: '2-digit', minute:'2-digit', hour12: false});
+                    var arrStation = (result.timeTableRows[index].stationShortCode);
 
 
                     console.log(arrStation);
                     if (result.timeTableRows[i].type === "DEPARTURE") { //tulostetaan vain departures
-                    timetable = timetable + "<div class=\"trips\" \"trip\""+i+"\">" + "Määräasema: " + arrStation + " Lähtöaika: " + deptTime + " Saapumisaika: " + arrTime + "</div>";
-                        for(var k = 0; k < index; k++) {
-                            if (result.timeTableRows[i].type === "DEPARTURE"){
-                            timetable = timetable + "<div class=\"stops\" \"stop\""+i+"\">"+ result.timeTableRows[k].stationShortCode+" - " + result.timeTableRows[k].scheduledTime + "</div>";
-                            }
-                        }
+                    timetable = timetable + "<div class=\"trips trip"+i+"\">" + "Määräasema: " + arrStation + " Lähtöaika: " + deptTime + " Saapumisaika: " + arrTime + "</div>";
+                  //  for(var k = 0; k <= index; k++) {
+                  //         timetable = timetable + "<div class=\"stops stop" + i + "\">" + result.timeTableRows[i].type + " - " + result.timeTableRows[k].stationShortCode + " - " + result.timeTableRows[k].scheduledTime + "</div>";
+                  //      }
                     }
 
 
