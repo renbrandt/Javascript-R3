d7bd37588f8e70c9b938a9271768e4b2384eb59c
outiarimaa <outi.arimaa@gmail.com>
2018-02-14 12:43:54 +0200

    Lisätty kirjautuminen HTML ja JS

--- src/junat.js
+++ src/junat.js
@@ -62,3 +62,33 @@
         }
     }
 
+    // Name and Password from the register-form
+    var username = document.getElementById('name1');
+    var pw = document.getElementById('pw');
+
+    // storing input from register-form
+    function store() {
+        localStorage.setItem('username', username.value);
+        localStorage.setItem('pw', pw.value);
+    }
+
+    // check if stored data from register-form is equal to entered data in the   login-form
+    function check() {
+
+        // stored data from the register-form
+        var storedName = localStorage.getItem('username');
+        var storedPw = localStorage.getItem('pw');
+
+        // entered data from the login-form
+        var userName = document.getElementById('userName');
+        var userPw = document.getElementById('userPw');
+
+        // check if stored data from register-form is equal to data from login form
+        if (userName.value == storedName && userPw.value == storedPw) {
+            alert('You are logged in.');
+        } else {
+            alert('ERROR.');
+        }
+
+    }
+
