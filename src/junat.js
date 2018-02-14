c7dbef8e0d2d029765b3039f97f24736d7ecd4f7
Tiinakooo <tiina.hrd@gmail.com>
2018-02-14 10:21:40 +0200

    Lisätty junien tyypit js fileen

--- src/junat.js
+++ src/junat.js
@@ -25,14 +25,34 @@
 
                     var index = indexSearch(result);
 
+                    var trainCategory = result.trainCategory;
+                        if (trainCategory === "Long-distance"){
+                            if (result.trainType === "IC"){
+                                var trainNumber = "Kaukojuna " + "InterCity " + result.trainType + result.trainNumber;
+                            }else if (result.trainType === "IC2"){
+                                    trainNumber = "Kaukojuna " + "InterCity 2 " + result.trainType + result.trainNumber;
+                            } else if (result.trainType === "P"){
+                                    trainNumber = "Kaukojuna " + "Pikajuna " + result.trainType + result.trainNumber;
+                            } else if (result.trainType === "S"){
+                                    trainNumber = "Kaukojuna " + "Pendolino " + result.trainType + result.trainNumber;
+                            } else if (result.trainType = "AE"){
+                                    trainNumber = "Kaukojuna " + "Allegro " + result.trainType + result.trainNumber;
+                            }
+
+                        } else if (trainCategory === "Commuter") {
+                            trainNumber = "Lähijuna " + result.commuterLineID;
+                        }
+
+
                     var deptTime = new Date(result.timeTableRows[0].scheduledTime).toLocaleTimeString("fi", {hour: '2-digit', minute:'2-digit', hour12: false});
                     var arrTime = new Date(result.timeTableRows[index].scheduledTime).toLocaleTimeString("fi", {hour: '2-digit', minute:'2-digit', hour12: false});
                     var arrStation = (result.timeTableRows[index].stationShortCode);
 
 
+
                     console.log(arrStation);
                     if (result.timeTableRows[i].type === "DEPARTURE") { //tulostetaan vain departures
-                    timetable = timetable + "<div class=\"trips trip"+i+"\">" + "Määräasema: " + arrStation + " Lähtöaika: " + deptTime + " Saapumisaika: " + arrTime + "</div>";
+                    timetable = timetable + "<div class=\"trips trip"+i+"\">" + trainNumber + " Lähtöaika: " + deptTime + " Saapumisaika: " + arrTime + "</div>";
                   //  for(var k = 0; k <= index; k++) {
                   //         timetable = timetable + "<div class=\"stops stop" + i + "\">" + result.timeTableRows[i].type + " - " + result.timeTableRows[k].stationShortCode + " - " + result.timeTableRows[k].scheduledTime + "</div>";
                   //      }
